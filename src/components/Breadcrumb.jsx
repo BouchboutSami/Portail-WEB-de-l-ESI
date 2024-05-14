6ca4cdfafbc0dfcfb045a8c601030a07a59c615a
Sarahbr1 <ks_brahiti@esi.dz>
2024-05-14 11:39:34 +0100

    Article card component + uni events page

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -36,19 +36,6 @@ const Breadcrumb = () => {
                 <div>{path}</div>
               )}
             </a>
-            {index < pathNames.length - 1 && (
-              <svg
-                className="fill-current w-3 h-3 mx-2 text-gray-500"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9.293 4.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H4a1 1 0 010-2h10.586L9.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            )}
           </li>
         ))}
       </ol>
--- src/components/PostImage.jsx
+++ src/components/PostImage.jsx
@@ -11,12 +11,12 @@ const PostImage = (props) => {
         className="object-cover w-full h-full absolute inset-0"
       />
       <div className="absolute bottom-0 left-0 w-full opacity-90 h-[300px] bg-gradient-to-t from-black " />
-      <div className="absolute bottom-8 w-full font-poppins font-medium text-white ">
+      <div className="absolute bottom-8 w-full font-poppins text-white ">
         <div className="flex flex-col">
           <h1 className="text-[48px] font-extrabold text-white">
             {props.titre}
           </h1>
-          <p className="text-[16px] font-medium text-white w-[600px]">
+          <p className="text-[16px] font-extralight text-white w-[600px]">
             {props.details}
           </p>
         </div>
