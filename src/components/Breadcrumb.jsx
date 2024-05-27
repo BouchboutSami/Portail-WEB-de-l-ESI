8d3b9b4f7be4f7a81e64b6c6d15db161a1efc2ee
Selbou <ks_boughoufalah@esi.dz>
2024-05-27 11:41:00 +0100

    structure pour sami

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -29,26 +29,13 @@ const Breadcrumb = () => {
             >
               {pathNames.length - 1 !== index ? (
                 <div className="flex flex-row gap-2 items-center">
-                  {path}
+                  {path.replace("-", " ")}
                   <LuChevronRight className=" h-4" />
                 </div>
               ) : (
                 <div>{path}</div>
               )}
             </a>
-            {index < pathNames.length - 1 && (
-              <svg
-                className="fill-current w-3 h-3 mx-2 text-gray-500"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9.293 4.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H4a1 1 0 010-2h10.586L9.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            )}
           </li>
         ))}
       </ol>
