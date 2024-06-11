bbf251bd0e91ce77b272f491479e43310aaae6d7
Novae <kw_asselah@esi.dz>
2024-06-11 23:49:21 +0100

    Commit Reglementations

--- src/components/TelechargerDoc.jsx
+++ src/components/TelechargerDoc.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { IoDocumentOutline } from "react-icons/io5";
-const TelechargerDoc = ({ text, link }) => {
+const TelechargerDoc = ({ text, link, additionalStyles }) => {
     return (
-        <div className="relative my-[1%] ">
+        <div className={`relative my-[1%] ${additionalStyles}`}>
             <div className="flex items-center">
                 <button
                     className="text-black bg-white border min-w-[50%] border-gray-400 shadow-sm relative z-10"
