0e35a9293f38c12a7cb551c3cfb036bfd1eab39f
Melissa Benchelli <km_benchelli@esi.dz>
2024-06-12 10:28:47 +0100

    melissa page research et accreditation

--- src/app/Postgraduation-Research/Academic-authorization/page.js
+++ src/app/Postgraduation-Research/Academic-authorization/page.js
@@ -4,19 +4,90 @@ import NavBar from "../../../Components/NavBar";
 import PostImage from "../../../Components/PostImage";
 import PostSideBar from "../../../Components/PostSideBar";
 import { BrowserRouter } from "react-router-dom";
+import CampusNewsCard from "@/components/CampusNewsCard";
+import { IoIosArrowForward } from "react-icons/io";
 
 const page = () => {
+  const News = [
+    {
+      image: "/assets/HabilitationAUF.png",
+      title:
+        "FAQ: Journée mondiale de l’alimentation",
+      link: "/",
+    },
+    {
+      image: "/assets/HabilitationAUF.png",
+      title:
+        "Prix de thèses francophones de prospection, édition 2022",
+      link: "/",
+    },
+    {
+      image: "/assets/HabilitationAUF.png",
+      title:
+        "2ème Assise de la francophonie scientifique",
+      link: "/",
+    },
+    {
+      image: "/assets/HabilitationAUF.png",
+      title:
+        "Une équipe de l'ESI remporte le Hackathon Alger Legal Tech Hackathon",
+      link: "/",
+    },
+    {
+      image: "/assets/HabilitationAUF.png",
+      title:
+        "Une équipe de l'ESI remporte le Hackathon Alger Legal Tech Hackathon",
+      link: "/",
+    },
+    {
+      image: "/assets/HabilitationAUF.png",
+      title:
+        "Une équipe de l'ESI remporte le Hackathon Alger Legal Tech Hackathon",
+      link: "/",
+    },];
   return (
+    
     <BrowserRouter>
       <div className=" w-screen">
         <NavBar />
-        <PostImage
+        <PostImage image="/assets/ResearchProjects.png"
         titre = 'Academic authorization' 
         details='Explore academic authorization at ESI. Join us to keep up to date with all the latest accreditations and news.' />
         <div className="flex ">
           <PostSideBar />
           <div className=" w-full items-center text-center">
-            academic authorization
+          <div className="text-black font-poppins text-[20px] mt-6 px-8 py-8">
+          Understanding accreditation not only sheds light on the credibility and 
+          legitimacy of educational offerings but also illuminates the broader landscape of 
+          academia and its continuous pursuit of improvement.
+          </div>
+          <div className="w-full flex justify-center items-center flex-col">
+            <p className="text-black font-poppins font-bold text-[20px] mb-4">University accreditation</p>
+            <div className="flex justify-center items-center gap-5">
+              <CampusNewsCard title="Annonces" image="/assets/HabilitationAnnonce.png"/>
+              <CampusNewsCard title="Guide" image="/assets/HabilitationGuide.png"/>
+              <CampusNewsCard title="Documents"image="/assets/HabilitationDocs.png" />
+            </div>
+         </div>
+
+         <div className="bg-[#F5F5F5] w-full px-[5%] mt-4">
+         <p className="text-black font-poppins font-bold text-[20px] mb-4">Articles News </p>
+         <div className="grid grid-cols-3 gap-4 items-center pb-4">
+          {/* max 8 elements to display in the section  */}
+          {News.slice(0, 8).map((item, index) => (
+          <a href={item.link} key={index}>
+            <CampusNewsCard title={item.title} image={item.image} />
+          </a>
+           ))}
+          </div>
+          <div className="flex justify-end">
+          <a href="" className="flex items-center">
+          <span className="mr-1 text-[#185B9C]">Découvrir plus</span>
+          <IoIosArrowForward className="w-4 h-4 text-[#185B9C] ml-1" /> 
+          </a>
+          </div>
+        </div>
+
           </div>
         </div>
       </div>
--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -24,23 +24,6 @@ const ArticleCard = ({ imageUrl, title, description, link, linkText }) => {
       <div className=" mt-2 mb-2 w-full h-[1px] bg-gray-400 opacity-60"></div>
     </div>
   );
-        <div className="flex flex-row justify-center pb-4">
-            <div className=" pr-4">
-                <img src={imageUrl} alt={title} className="w-[300px] h-auto" />
-            </div>
-            <div className="w-1/2 flex flex-col">
-                <p className="font-black text-lg mt-2 text-black">{title}</p>
-                <p className="text-base font-normal text-black">{description}</p>
-                <div className="flex mt-2">
-                    <a href={link} className="flex font-poppins font-bold text-amber-800 hover:underline">
-                {linkText} <RxCaretRight className="mt-1" />
-                </a>
-                </div>
-            </div>
-        </div>
-        <div className=" mt-2 mb-2 w-full h-[1px] bg-gray-400 opacity-60"></div>
-    </div>
-  )
 };
 
 export default ArticleCard;
