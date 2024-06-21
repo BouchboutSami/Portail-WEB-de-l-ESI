6000605878b9a7106eeb0894450e0af30776a329
Bouchbout-sami <ks_bouchbout@esi.dz>
2024-06-21 05:38:10 +0100

    first deploy

--- src/app/YouAre-Staff/page.js
+++ src/app/YouAre-Staff/page.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import StaffImage from "@/components/StaffImage";
