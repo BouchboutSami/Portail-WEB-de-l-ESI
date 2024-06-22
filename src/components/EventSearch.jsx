6e209b4602317b2a934e7109a7fa4908d78ef271
Bouchbout-sami <ks_bouchbout@esi.dz>
2024-06-22 18:30:51 +0100

    test

--- src/components/EventSearch.jsx
+++ src/components/EventSearch.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const EventSearch = () => {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [selectedType, setSelectedType] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedType, setSelectedType] = useState("");
   const [selectedDate, setSelectedDate] = useState(null);
   const [showTypeFilter, setShowTypeFilter] = useState(false);
 
@@ -29,22 +29,24 @@ const EventSearch = () => {
           placeholder="Search for an event"
           value={searchQuery}
           onChange={handleSearchChange}
-          className='bg-transparent border-b-2 p-[1px]'
+          className="bg-transparent border-b-2 p-[1px]"
         />
       </div>
       <div className="filters flex gap-3 w-1/2">
-        <select name="" id="select" className='flex gap-3 w-1/2 p-2 rounded-lg'>
-        <option value="" disabled selected>Type</option>
-         <option value="">Conference</option>
-         <option value="">Soutenance</option>
-        <option value="">Hackathon</option>
+        <select name="" id="select" className="flex gap-3 w-1/2 p-2 rounded-lg">
+          <option value="" disabled selected>
+            Type
+          </option>
+          <option value="">Conference</option>
+          <option value="">Soutenance</option>
+          <option value="">Hackathon</option>
         </select>
-        <div className="filter w-1/2">
+        <div className="filter w-1/2 z-50 relative">
           <DatePicker
             selected={selectedDate}
             onChange={handleDateChange}
             placeholderText="Select Date"
-            className='flex gap-3 w-full p-2 rounded-lg'
+            className="flex gap-3 w-full p-2 rounded-lg z-50 relative"
           />
         </div>
       </div>
