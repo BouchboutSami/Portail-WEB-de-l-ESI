5d0a5cc4b4b79f1c7ac464decfdc79286a07d1a3
Bouchbout-sami <ks_bouchbout@esi.dz>
2024-06-22 18:21:55 +0100

    test

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -4,12 +4,12 @@ const Stats = (props) => {
   const stat = props.table;
   return (
     <div className="bg-[#185B9C] py-24 sm:py-14">
-      <div className="max-w-7xl">
-        <dl className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-16 text-center gap-x-0">
+      <div className="max-w-7xl flex justify-center">
+        <dl className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-16 text-center justify-center items-center gap-x-[5%]">
           {stat.map((item, index) => (
             <div
               key={index}
-              className="mx-auto flex max-w-xs justify-center flex-col gap-y-4"
+              className="mx-auto flex w-fit justify-center items-center flex-col gap-y-4"
             >
               <dt className="text-3xl font-semibold leading-7 text-white font-poppins">
                 {item.title}
