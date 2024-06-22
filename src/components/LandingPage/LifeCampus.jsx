a9465bb02093e38b156f808dcd19b15fe9682dfd
Bouchbout-sami <ks_bouchbout@esi.dz>
2024-06-22 17:44:21 +0100

    test

--- src/components/LandingPage/LifeCampus.jsx
+++ src/components/LandingPage/LifeCampus.jsx
@@ -34,7 +34,7 @@ const LifeCampus = () => {
         Find out about our range of activities designed to liven up student life
       </div>
       <div className="container  mx-auto mt-4">
-        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-fit">
           {photos.map((photo, index) => (
             <div
               key={index}
