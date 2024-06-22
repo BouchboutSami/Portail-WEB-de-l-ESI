a84f722d9f76a8b133b33de9ae2ff91ce9867f58
Bouchbout-sami <ks_bouchbout@esi.dz>
2024-06-22 02:08:44 +0100

    test

--- src/app/Services/Pre-promotionTraining/Planning/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>planning</div>;
-};
-
-export default page;
--- src/app/Services/Pre-promotionTraining/Syllabus/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>Syllabus</div>;
-};
-
-export default page;
