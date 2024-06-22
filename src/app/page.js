ad288891cca9ea878962237c74f96bffd81fd631
Bouchbout-sami <ks_bouchbout@esi.dz>
2024-06-22 17:55:06 +0100

    test

--- src/app/page.js
+++ src/app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
         <NextEvents />
       </div>
       <Academics />
-      <LifeCampus />
+      {/* <LifeCampus /> */}
       <PostGradAndResearchSection />
       <ServicesForCompanies />
       <Partenaires />
